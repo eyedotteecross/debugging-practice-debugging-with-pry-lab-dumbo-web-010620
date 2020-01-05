4c04400b9fd322a3dd6843c2b8674e85ea28ffa6
Anthony Holder <anthonyh202x@gmail.com>
2020-01-05 19:29:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,7 +4,7 @@ end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  "Captain\'s Log, star date #{star_date}."
 end
 
 def crew
@@ -17,6 +17,6 @@ end
 
 def engage
   date = generate_star_date
-  state_log(date)
+  state_log(star_date)
   greet_crew(crew)
 end
